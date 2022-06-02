7aaad2412f20d0008560bb8b8e65218dc19fffa8
thanhnguyenht303 <nguyenhuutienthanh2002@gmail.com>
2022-06-02 10:43:54 +0700

    home-page-banner

--- src/components/HomepageBanner.js
+++ src/components/HomepageBanner.js
@@ -17,26 +17,28 @@ export const HomepageBanner = ({
     
     <div>
       <section className='card'>
-        <h1 className='title'>End Poverty & Overcome Racism</h1>
+        <h4 className='title'>End Poverty & Overcome Racism</h4>
         <button className='banner-button'>Join the Fight</button>
       </section>
       <section className='group'>
         <text>
           01
-          <div className='retangle'></div>
-          <h1 className='title'>What is the American plan?</h1>
+          
+          <h4 className='title'>What is the American plan?</h4>
           <p>Read the full text of our American Plan to End Poverty and Overcome Racism</p>
         </text>
         <text>
           02
-          <div className='retangle'></div>
-          <h1 className='title'>Ten Points for Action</h1>
+          
+          <h4 className='title'>Ten Points for Action</h4>
           <p>The American Plan identifies 10 keys areas for tackling poverty and racism</p>
         </text>
         <text>
           03
-          <div className='retangle'></div>
-          <h1 className='title'>About Us</h1>
+          <div className='retangle' style={{backgroundColor:'orangered'}}></div>
+          <div className='retangle' style={{backgroundColor:'white'}}></div>
+          <div className='retangle' style={{backgroundColor:'gray'}}></div>
+          <h4 className='title'>About Us</h4>
           <p>The diverse ideas and people behind the American Plan</p>
         </text>
       </section>
