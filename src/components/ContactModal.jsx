606460b5080c35ef302f5f03a83823e4c89c23b8
thanhnguyenht303 <nguyenhuutienthanh2002@gmail.com>
2022-06-06 17:38:55 +0700

    contact-modal

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 import { deepMemo } from '@hooks';
-import { Modal } from '@mui/material';
-import {Box} from '@mui/material'
-import { TextField } from '@mui/material';
-import contactImg from './images/contat-modal.png'
+import {Modal, Box, TextField} from '@mui/material'
+import { makeStyles } from '@mui/styles';
+import contact_modal from '@images/contact-modal.png';
+import { borderRadius } from '@mui/system';
 
-const styles = theme => ({
-    textField: {
-        width: '90%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        color: 'white',
-        paddingBottom: 0,
-        marginTop: 0,
-        fontWeight: 500
-    },
-});
+const useSytle = makeStyles(() => (
+   {textField: {
+    width: "80%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    paddingBottom: 0,
+    marginTop: 0,
+    fontWeight: 500,
+  },
+  input: {
+    color: "blue"
+  }}))
+
+const textStyle = makeStyles(() => (
+    {textField2:{
+            width: "39%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            paddingBottom: 0,
+            marginTop: 0,
+            fontWeight: 500,
+    }}))
 
 const ContactModal = () => {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const classes = useSytle();
+    const newClass = textStyle();
+
     return (
         <div>
             <button className='banner-button' onClick={handleOpen}>Join the Fight</button>
@@ -28,46 +42,63 @@ const ContactModal = () => {
                 open = {open}
                 aria-describedby = "form"
             >
-                <Box 
-                    sx ={{
-                        bgcolor: "background.paper",
-                        width: 1075,
-                        height: 636,
-                        position: 'relative',
-                        top: "10%",
-                        left: "15%",
-                    }}>
-                        <img src={contactImg}/>
-                        <button className='btn' onClick={handleClose} style={{fontSize:24}}>X</button>
+                <div className='container' >
+                    <div className='item-a'>
                         <Box 
-                            component="form"
-                            sx={{
-                                '& .MuiTextField-root':{m: 3},
-
-                            }}
-                            noValidate
-                            autoComplete = "off">
-                            <div id = "form">   
-                                <h1 style={{color: "orangered", 
-                                        fontFamily:"sans-serif" }}>It's your turn to take action</h1>         
-                                <TextField 
-                                    id= "outlined-first-name" label = "First Name*" type = "text" placeholder='First Name'
-                                />
-                                <TextField sx={{}}
-                                    id = "outlined-last-name" label = "Last Name*" type = "text" placeholder='Last Name'
-                                />
-                                <TextField 
-                                    id = "outlined-email" label = "Email*" type = "text" placeholder='Email'
-                                />
-                                <TextField 
-                                    id = "outlined-subjects" label = "Subjects" type = "text" placeholder='Subjects'
-                                />
-                                <TextField 
-                                    id = "outlined-messages" label = "Messages" type = "text" placeholder='Messages' multiline rows={5}
-                                />
-                            </div>
-                        </Box>
-                    </Box>
+                            sx ={{
+                                bgcolor: "background.paper",
+                                width: 427,
+                                height: 636,
+                                position: 'relative',
+                                top: "10%",
+                                left: "10%",
+                            }}>            
+                            <img src = {contact_modal} alt = ''/>
+                        </Box> 
+                    </div>
+                    <div className='item-b'>
+                        <Box 
+                            sx ={{
+                                bgcolor: "background.paper",
+                                width: 648,
+                                height: 636,
+                                position: 'relative',
+                                top: "10%",
+                                left: "10%",
+                            }}>   
+                            <button className='btn' onClick={handleClose} style={{fontSize:24}}>X</button>
+                            <Box 
+                                component="form"
+                                sx={{
+                                '& .MuiTextField-root':{m: 1},
+                                }}
+                                noValidate
+                                autoComplete = "off">
+                                <div id = "form">   
+                                    <h1 style={{color: "orangered", 
+                                        fontFamily:"sans-serif" }}>It's your turn to take action</h1>
+                                    <h2> Join us in our commitment to create lasting solution to poverty, hunger, and social injustice </h2>         
+                                    <TextField 
+                                        id= "outlined-first-name" label = "First Name*" type = "text" placeholder='First Name' className={newClass.textField2}
+                                    />
+                                    <TextField sx={{}}
+                                        id = "outlined-last-name" label = "Last Name*" type = "text" placeholder='Last Name' className={newClass.textField2}
+                                    />
+                                    <TextField 
+                                        id = "outlined-email" label = "Email*" type = "text" placeholder='Email' className={classes.textField}
+                                    />
+                                    <TextField 
+                                        id = "outlined-subjects" label = "Subjects" type = "text" placeholder='Subjects' className = {classes.textField}
+                                    />
+                                    <TextField 
+                                        id = "outlined-messages" label = "Messages" type = "text" placeholder='Messages' multiline rows={5} className={classes.textField}
+                                    />
+                                </div>
+                                <button className='submit-btn' style={{top:'95%', left:'65%'}}>Submit</button>
+                            </Box>
+                        </Box>        
+                    </div>
+                </div>
             </Modal>
         </div>
     )
