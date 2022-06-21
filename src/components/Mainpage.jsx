2ce29bf0bfdf1e4ad433948a9bada71a823adb4c
thanhnguyenht303 <nguyenhuutienthanh2002@gmail.com>
2022-06-21 13:34:31 +0700

    mainpage

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -32,6 +32,13 @@ const itemStyle = makeStyles(()=> ({
         paddingLeft: 39,
         width: 201,
         height: 42,
+        lineHeight: 1.35
+    },
+
+    itemContent: {
+        fontFamily: "Karla", 
+        fontSize: 18, 
+        fontWeight: "bold", 
     },
 
     circle: {
@@ -39,12 +46,17 @@ const itemStyle = makeStyles(()=> ({
         width: 50,
         height: 50,
         borderRadius: "50%",
+        position: "absolute", 
+        textAlign: "center", 
+        alignItems: "center", 
+        justifySelf: "center",
     },
 
     circleContent: {
         color: "#cf1111",
         fontWeight: "bold",
         fontSize: 24,
+        fontFamily: "Roboto",
     }
 }))
 
@@ -58,7 +70,7 @@ const mainpage = () => {
         <Main  backgroundUrl = {background}>
             <link href='https://fonts.googleapis.com/css?family=Karla' rel='stylesheet' />
             <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' />
-            
+           
             <div className="title">
                
                 <Box style={{
@@ -103,14 +115,82 @@ const mainpage = () => {
             <div className="items">
                 <Box className = {classes.root} style={{ top: 944, left: 61, }}>
                     <Box className = {classes.content}>
-                    <a style={{
-                        fontFamily: "Karla", 
-                        fontSize: 18, 
-                        fontWeight: "bold", 
-                        lineHeight: "normal",}}> Create decent housing for all </a>
+                        <a className = {classes.itemContent}> Create decent housing for all </a>
                     </Box>
-                    <Box className = {classes.circle} style = {{right: 230, top: 89, position: "absolute", textAlign: "center", justifyItem: "center"}}>
-                        <a className = {classes.circleContent} style = {{fontFamily: "Roboto", }}> 1 </a>
+                    <Box className = {classes.circle} style = {{right: 230, top: 89, }}>
+                        <h1 className = {classes.circleContent}> 1 </h1>
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 908, left: 443, }}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> Achieve equity in school funding </a>
+                    </Box>
+                    <Box className = {classes.circle} style = {{right: 234, top: 94}}>
+                        <h1 className = {classes.circleContent}> 2 </h1>
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 992, left: 800}}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> Provide employment or universal basic income </a>
+                    </Box>
+                    <Box className = {classes.circle} style = {{right: 235, top: 94}}>
+                        <h1 className = {classes.circleContent}> 3 </h1>
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 898, left: 1124}}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> Conduct policy, and deliver public service </a>
+                    </Box>
+                    <Box className = {classes.circle} style = {{right: 235, top: 94}}>
+                        <h1 className = {classes.circleContent}> 4 </h1> 
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 1093, left: 1145}}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> Ensure enough stores and offices </a> 
+                    </Box>
+                    <Box className = {classes.circle} style = {{right: 234, top: 90}}>
+                        <h1 className = {classes.circleContent}> 5 </h1> 
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 1291, left: 1080}}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> Health care coverage & a clean environment </a>
+                    </Box>
+                    <Box className = {classes.circle} style = {{right: 237, top: 98}}>
+                        <h1 className = {classes.circleContent}> 6 </h1>
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 1435, left: 808,}}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> Assure full voting rights </a>
+                    </Box>
+                    <Box className = {classes.circle} style = {{right: 228, top: 94}}>
+                        <h1 className = {classes.circleContent}> 7 </h1>
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 1305, left: 518,}}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> End the transfer of military hardware </a>
+                    </Box>
+                    <Box className = {classes.circle} style = {{right: 232, top: 94}}>
+                        <h1 className = {classes.circleContent}> 8 </h1>
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 1401, left: 216,}}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> Eliminate arrest for drug offenses </a>
+                    </Box>
+                    <Box className = {classes.circle} style = {{top: 93, right: 227,}}>
+                        <h1 className = {classes.circleContent}> 9 </h1>
+                    </Box>
+                </Box>
+                <Box className = {classes.root} style = {{top: 1186, left: 23}}>
+                    <Box className = {classes.content}>
+                        <a className = {classes.itemContent}> Etablish diverse commisions </a>
+                    </Box>
+                    <Box className = {classes.circle} style = {{top: 99, right: 232}}>
+                        <h1 className = {classes.circleContent}> 10 </h1>
                     </Box>
                 </Box>
             </div>
